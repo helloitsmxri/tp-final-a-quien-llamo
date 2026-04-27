c2cda82e109898ff0956efea8818a4efcc56c9fc
Lour <mariaelencow@hotmail.com>
2026-04-27 14:22:40 -0300

    01. Fusión con Spring Boot
    (Queda en el main para que todas tengamos el main de la aplicación)

---
+++ aquienllamo/aquienllamo/src/main/java/com/aquienllamo/aquienllamo/AquienllamoApplication.java
@@ -0,0 +1,13 @@
+package com.aquienllamo.aquienllamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquienllamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquienllamoApplication.class, args);
+	}
+
+}
---
+++ aquienllamo/aquienllamo/src/test/java/com/aquienllamo/aquienllamo/AquienllamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquienllamo.aquienllamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquienllamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
