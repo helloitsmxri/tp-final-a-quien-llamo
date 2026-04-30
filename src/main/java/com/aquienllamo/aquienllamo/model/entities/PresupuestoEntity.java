7afaaedc9226442f2147275a98009b4b47c4b097
Lour <mariaelencow@hotmail.com>
2026-04-30 11:49:20 -0300

    Creación PresupuestoEntity
    Creé la entidad presupuesto

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/entities/PresupuestoEntity.java
@@ -0,0 +1,34 @@
+package com.aquienllamo.aquienllamo.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Table(name = "Presupuesto")
+public class PresupuestoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_presupuesto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private UsuarioEntity id_usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tecnico", nullable = false)
+    private TecnicoEntity id_tecnico;
+
+    @Column(name = "precio_estimado", precision = 10, scale = 2, nullable = false)
+    private BigDecimal precio_estimado;
+
+    @Column(name = "descripcion_presupuesto", columnDefinition = "TEXT", nullable = false)
+    private String descripcion_presupuesto;
+
+}
