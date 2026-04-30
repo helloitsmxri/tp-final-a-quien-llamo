56a740b14d7d3e3f84abe3dbdf8e01a8e9a8fa71
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-04-30 12:11:43 -0300

    actualice TrabajoEntity y agregue relaciones ManyToMany

--- src/main/java/com/aquienllamo/aquienllamo/model/entities/TecnicoEntity.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/entities/TecnicoEntity.java
@@ -3,6 +3,8 @@ package com.aquienllamo.aquienllamo.model.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,18 +16,37 @@ import lombok.*;
 public class TecnicoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id_tecnico;
+    @Column(name = "id_tecnico")
+    private Integer idTecnico;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
-    private UsuarioEntity id_usuario;
+    private UsuarioEntity usuario;
 
     @Column(nullable = false, length = 20)
     private String cuit;
 
-    @Column(columnDefinition = "TEXT")
-    private String descripcion_trabajo;
+    @Column(name = "descripcion_trabajo", columnDefinition = "TEXT")
+    private String descripcionTrabajo;
 
     @Column(columnDefinition = "TEXT")
     private String proyectos;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "Habilidad_Tecnico",
+            joinColumns = @JoinColumn(name = "id_tecnico"),
+            inverseJoinColumns = @JoinColumn(name = "id_habilidad")
+
+    )
+    private List<HabilidadEntity> habilidades;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "Especialidad_Tecnico",
+            joinColumns = @JoinColumn(name = "id_tecnico"),
+            inverseJoinColumns = @JoinColumn(name = "id_especialidad")
+    )
+    private List<EspecialidadEntity> especialidades;
+
 }
