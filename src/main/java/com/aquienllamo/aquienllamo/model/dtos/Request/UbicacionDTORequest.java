b44844b1921e428b5f4f52351cd2310d025aad2a
Paulina006 <paulinasaffer5@gmail.com>
2026-05-05 11:55:37 -0300

    Ubicacion DTO
    hice los dtos de la clase Ubicación, le agregué el Request y Response

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/UbicacionDTORequest.java
@@ -0,0 +1,24 @@
+package com.aquienllamo.aquienllamo.model.dtos.Request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor // constructor completo
+@NoArgsConstructor // constructor vacío
+@ToString
+
+//Request: es lo que el usuario me manda.
+public class UbicacionDTORequest {
+    private Integer idUbicacion;
+    private String codigoPostal;
+    private String provincia;
+    private String ciudad;
+    private String calle;
+    private Integer numero;
+    private Integer piso;
+    private Integer numeroPiso;
+
+
+
+}
