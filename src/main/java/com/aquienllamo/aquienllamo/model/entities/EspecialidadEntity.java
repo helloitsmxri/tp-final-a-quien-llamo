3655d15d818273492fd352c823bbee1e289ca4de
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-07 08:39:18 -0300

    update EspecialidadEntity
    le agregue la columna uuid la cual va a tener un id externo para el front

--- src/main/java/com/aquienllamo/aquienllamo/model/entities/EspecialidadEntity.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/entities/EspecialidadEntity.java
@@ -4,6 +4,8 @@ import com.aquienllamo.aquienllamo.model.Enum.TipoValidacion;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +19,16 @@ public class EspecialidadEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_especialidad")
     private Integer idEspecialidad;
+
+    @Column(name = "uuid", nullable = false, unique = true)
+    private String uuid;
+
+    @PrePersist
+    public void generarUUID() {
+        if (this.uuid == null) {
+            this.uuid = UUID.randomUUID().toString();
+        }
+    }
 
     @Column(length = 50, name = "nombre_especialidad")
     private String nombreEspecialidad;
