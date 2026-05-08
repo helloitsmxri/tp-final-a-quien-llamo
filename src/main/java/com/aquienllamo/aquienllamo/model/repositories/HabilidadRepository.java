6dd9eeea9c8d97b77eec472b0e2cf028745c27c8
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-08 19:59:50 -0300

    cree HabilidadRepository

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/HabilidadRepository.java
@@ -0,0 +1,12 @@
+package com.aquienllamo.aquienllamo.model.repositories;
+
+import com.aquienllamo.aquienllamo.model.entities.HabilidadEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface HabilidadRepository {
+    Optional<HabilidadEntity> findByUuid(String uuid);
+    boolean existsByNombreHabilidad(String nombreHabilidad);
+}
