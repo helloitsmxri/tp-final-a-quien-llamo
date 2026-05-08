5a8660281fc901956966e6aa32ac2d568f5d9ad2
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-08 20:52:08 -0300

    cree la excepcion EspecialidadNotFoundEx

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/EspecialidadNotFoundEx.java
@@ -0,0 +1,8 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class EspecialidadNotFoundEx extends RuntimeException {
+    public EspecialidadNotFoundEx(String message) {
+        super(message);
+    }
+    public EspecialidadNotFoundEx() {}
+}
