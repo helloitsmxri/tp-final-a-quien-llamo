16814756d011309561f2161e1a118424aa125330
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-08 19:47:50 -0300

    cree TecnicoRepository

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/TecnicoRepository.java
@@ -0,0 +1,18 @@
+package com.aquienllamo.aquienllamo.model.repositories;
+
+import com.aquienllamo.aquienllamo.model.entities.TecnicoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TecnicoRepository extends JpaRepository<TecnicoEntity,Integer> {
+    Optional<TecnicoEntity> findByUuid(String uuid);
+    Optional<TecnicoEntity> findByCuit(String cuit);
+    boolean existsByCuit(String cuit);
+    boolean existsByUsuario_idUsuario(Integer idUsuario);
+    List<TecnicoEntity> findByHabilidades_IdHabilidad(Integer idHabilidad);
+    List<TecnicoEntity> findByEspecialidades_IdEspecialidad(Integer idEspecialidad);
+}
