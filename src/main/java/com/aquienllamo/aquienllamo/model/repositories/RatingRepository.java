1e97c7ca8c13cd7489c9c92cd634b5af0ec170c4
IrinaB7 <irinabenvenutto@gmail.com>
2026-05-11 20:09:03 -0300

    update rating

--- src/main/java/com/aquienllamo/aquienllamo/model/repositories/RatingRepository.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/RatingRepository.java
@@ -3,11 +3,11 @@ package com.aquienllamo.aquienllamo.model.repositories;
 
 import com.aquienllamo.aquienllamo.model.entities.RatingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface RatingRepository extends JpaRepository<RatingRepository, Integer> {
 
     //ver todas las reseñas de un tecnico/usuario
@@ -16,14 +16,4 @@ public interface RatingRepository extends JpaRepository<RatingRepository, Intege
     //ver todas las reseñas hechas por un usuario
     List<RatingEntity> findByIdSenderUser (Integer idUser);
 
-    //promedio de valoracion total
-    //SELECT AVG(r.valoracion) -> calcula promedio
-    //FROM RatingEntity r -> se una la entidad no la tabla de la bdd
-    //r.usuarioDestinatario.idUsuario -> el id del usuario que recibio la reseña
-    //:idUsuario -> el valor que remplada spring
-    @Query(""" 
-            SELECT AVG(r.valoracion)
-            FROM RatingEntity r
-            WHERE r.usuarioDestinatario.idUsuario = :idUsuario""")
-    Double averageRating(Integer idUser);
 }
