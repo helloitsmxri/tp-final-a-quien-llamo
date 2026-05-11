3e5e8e3b3146dedef006296820602bb5167f7528
IrinaB7 <irinabenvenutto@gmail.com>
2026-05-11 20:06:28 -0300

    update rubro

--- src/main/java/com/aquienllamo/aquienllamo/model/repositories/RubroRepository.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/RubroRepository.java
@@ -2,9 +2,11 @@ package com.aquienllamo.aquienllamo.model.repositories;
 
 import com.aquienllamo.aquienllamo.model.entities.RubroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RubroRepository extends JpaRepository<RubroEntity, Integer> {
 
     //buscar un rubro segun su uuid
