104f949823c578b6b0c733debd576e7d5653793a
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-11 19:15:09 -0300

    cree la excepcion EspecialidadAlreadyExistsE

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/EspecialidadAlreadyExistsE.java
@@ -0,0 +1,8 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class EspecialidadAlreadyExistsE extends RuntimeException {
+    public EspecialidadAlreadyExistsE(String message) {
+        super(message);
+    }
+    public EspecialidadAlreadyExistsE() {}
+}
