0e6f810e2c59a0e1013bed017678413f1c1fa3e0
Irinab7 <irinabenvenutto@gmail.com>
2026-05-26 20:39:10 -0300

    update portfolio
    corregi los dtos

--- src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/PortfolioAdminDTORequest.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/PortfolioAdminDTORequest.java
@@ -1,6 +1,9 @@
 package com.aquienllamo.aquienllamo.model.dtos.Request;
 
 import com.aquienllamo.aquienllamo.model.Enum.EstadoVerificacion;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @AllArgsConstructor
@@ -11,6 +14,10 @@ import lombok.*;
 @Builder
 public class PortfolioAdminDTORequest {
     //lo que el admin le manda al usuario
+    @NotNull(message = "Debe seleccionar un estado.")
     private EstadoVerificacion estadoVerificacion;
+
+    @NotBlank(message = "Este campo no debe quedar vacio.")
+    @Size(max = 2000, message = "Las notas no pueden superar los 2000 caracteres.")
     private String notasAdmin;
 }
--- src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/PortfolioUsuarioDTORequest.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/PortfolioUsuarioDTORequest.java
@@ -1,6 +1,8 @@
 package com.aquienllamo.aquienllamo.model.dtos.Request;
 
 import com.aquienllamo.aquienllamo.model.Enum.EstadoVerificacion;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 //que datos manda el usuario para el backend, en este caso el usuario esta creando el portfolio
@@ -12,11 +14,22 @@ import lombok.*;
 @Builder
 public class PortfolioUsuarioDTORequest {
 
-    private Integer especialidad; //esto se supone que en un futuro nosotros vamos a mostrar el nombre
+    @NotBlank(message = "Este campo no debe quedar vacio.")
+    private String uuidEspecialidad; //esto se supone que en un futuro nosotros vamos a mostrar el nombre
                                   //al usuario y lo que elija es el numero
+
+    @NotBlank(message = "Este campo no debe quedar vacio.")
+    @Size(max = 5000, message = "La nota no debe de ser de mas de 5000 caracteres.")
     private String notasAspirante;
+
+    @NotBlank(message = "Este campo no debe quedar vacio.")
+    @Size(max = 1000, message = "El enlace no debe de ser de mas de 1000 caracteres.")
     private String enlaceExterno;
+
+    @NotBlank(message = "Este campo no debe quedar vacio.")
+    @Size(max = 100, message = "El tipo de archivo no debe de ser de mas de 100 caracteres.")
     private String tipoArchivo;
+
     private byte[] archivoAdjunto;
 
 }
