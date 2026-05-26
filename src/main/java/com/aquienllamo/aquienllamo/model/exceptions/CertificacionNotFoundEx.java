e4793ad3ed517902a9ab0b8121656d713020afe0
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-26 17:05:03 -0300

    cree la excepcion CertificacionNotFoundEx

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/CertificacionNotFoundEx.java
@@ -0,0 +1,7 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class CertificacionNotFoundEx extends RuntimeException {
+    public CertificacionNotFoundEx(String message) {
+        super(message);
+    }
+}
