11793b46ddfa5be3f2875bfd34c049af174be0fa
Irinab7 <irinabenvenutto@gmail.com>
2026-05-26 19:47:53 -0300

    update pago
    cree el mapper de pago

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/mappers/PagoMapper.java
@@ -0,0 +1,29 @@
+package com.aquienllamo.aquienllamo.model.mappers;
+
+import com.aquienllamo.aquienllamo.model.dtos.Request.PagoDTORequest;
+import com.aquienllamo.aquienllamo.model.dtos.Response.PagoDTOResponse;
+import com.aquienllamo.aquienllamo.model.entities.PagoEntity;
+import com.aquienllamo.aquienllamo.model.entities.TrabajoEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagoMapper {
+
+    public static PagoDTOResponse toResponse (PagoEntity entity){
+        return PagoDTOResponse.builder()
+                .uuid(entity.getUuid())
+                .uuidTrabajo(entity.getTrabajo().getUuid())
+                .metodoDePago(entity.getMetodoDePago())
+                .fechaPago(entity.getFechaPago())
+                .estadoPago(entity.getEstadoPago())
+                .build();
+    }
+
+    public static PagoEntity toEntity (PagoDTORequest request, TrabajoEntity trabajo){
+        return PagoEntity.builder()
+                .trabajo(trabajo)
+                .metodoDePago(request.getMetodoDePago())
+                .build();
+    }
+
+}
