c6e1f7883eca5864c99894d23c822c3bee4b1e09
IrinaB7 <irinabenvenutto@gmail.com>
2026-05-26 09:05:57 -0300

    update trabajo request y creacion de mapper

--- src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/TrabajoDTORequest.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/dtos/Request/TrabajoDTORequest.java
@@ -3,6 +3,7 @@ package com.aquienllamo.aquienllamo.model.dtos.Request;
 import com.aquienllamo.aquienllamo.model.Enum.EstadoTrabajo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ public class TrabajoDTORequest {
     private String uuidPresupuesto;
 
     @NotBlank(message = "Justifique el presupuesto elegido.")
+    @Size(min = 50, max = 1000, message = "El mensaje debe tener minimo 50 caracteres, maximo 1000.")
     private String descripcionTrabajo;
 
     @NotNull(message = "Debe elegir una fecha para iniciar el trabajo.")
---
+++ src/main/java/com/aquienllamo/aquienllamo/model/mappers/TrabajoMapper.java
@@ -0,0 +1,33 @@
+package com.aquienllamo.aquienllamo.model.mappers;
+
+import com.aquienllamo.aquienllamo.model.dtos.Request.TrabajoDTORequest;
+import com.aquienllamo.aquienllamo.model.dtos.Response.TrabajoDTOResponse;
+import com.aquienllamo.aquienllamo.model.entities.PresupuestoEntity;
+import com.aquienllamo.aquienllamo.model.entities.TrabajoEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrabajoMapper {
+
+    public static TrabajoDTOResponse toResponse (TrabajoEntity entity){
+        return TrabajoDTOResponse.builder()
+                .uuid(entity.getUuid())
+                .uuidPresupuesto(entity.getPresupuesto().getUuid())
+                .descripcionTrabajo(entity.getDescripcionTrabajo())
+                .fechaEstimadaInicio(entity.getFechaEstimadaInicio())
+                .fechaEstimadaFin(entity.getFechaEstimadaFin())
+                .estadoTrabajo(entity.getEstadoTrabajo())
+                .build();
+    }
+
+    public static TrabajoEntity toEntity(TrabajoDTORequest request, PresupuestoEntity presupuesto){
+        return TrabajoEntity.builder()
+                .Presupuesto(presupuesto)
+                .descripcionTrabajo(request.getDescripcionTrabajo())
+                .fechaEstimadaInicio(request.getFechaEstimadaInicio())
+                .fechaEstimadaFin(request.getFechaEstimadaFin())
+                .estadoTrabajo(request.getEstadoTrabajo())
+                .build();
+    }
+
+}
