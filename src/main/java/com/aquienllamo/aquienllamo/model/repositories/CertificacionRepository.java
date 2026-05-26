a65af2a29276efe78af1ab101424d05c9726ec14
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-26 17:04:33 -0300

    cree la interfaz CertificacionRepository

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/CertificacionRepository.java
@@ -0,0 +1,14 @@
+package com.aquienllamo.aquienllamo.model.repositories;
+
+import com.aquienllamo.aquienllamo.model.entities.CertificacionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CertificacionRepository extends JpaRepository<CertificacionEntity, Integer>{
+    Optional<CertificacionEntity> findByUuid(String uuid);
+    List<CertificacionEntity> findByTecnico_Uuid(String uuidTecnico);
+}
