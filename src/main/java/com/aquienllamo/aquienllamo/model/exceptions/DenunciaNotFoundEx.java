4bdadecc9d3588212ab09a9747b1065804636185
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-28 11:08:21 -0300

    cree la excepcion DenunciaNotFound
    Co-Authored-By: Lour <141468124+helloitsmxri@users.noreply.github.com>

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/DenunciaNotFoundEx.java
@@ -0,0 +1,7 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class DenunciaNotFoundEx extends RuntimeException {
+    public DenunciaNotFoundEx(String message) {
+        super(message);
+    }
+}
