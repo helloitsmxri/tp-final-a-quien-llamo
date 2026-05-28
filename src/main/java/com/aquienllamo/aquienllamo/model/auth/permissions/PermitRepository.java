f65332864ffec96312e68960658c5b77635236aa
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-28 11:15:12 -0300

    reparacion
    Co-Authored-By: Lour <141468124+helloitsmxri@users.noreply.github.com>

--- src/main/java/com/aquienllamo/aquienllamo/model/auth/permissions/PermitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aquienllamo.aquienllamo.model.auth.permissions;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermitRepository extends JpaRepository<PermitEntity, Integer>{
-
-}
--- src/main/java/com/aquienllamo/aquienllamo/model/auth/permissions/Roles.java
+++
@@ -1,7 +0,0 @@
-package com.aquienllamo.aquienllamo.model.auth.permissions;
-
-public enum Roles{
-    ROLE_ADMINISTRADOR,
-    ROLE_USUARIO,
-    ROLE_TECNICO
-}
