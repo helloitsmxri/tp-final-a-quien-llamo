666b51aea3140fdb43efc2ab168f93818af1b2e7
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-05-30 17:21:22 -0300

    update DenunciaService
    modifique el metodo de crear

--- src/main/java/com/aquienllamo/aquienllamo/model/services/DenunciaService.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/services/DenunciaService.java
@@ -4,12 +4,15 @@ import com.aquienllamo.aquienllamo.model.Enum.EstadoDenunciaE;
 import com.aquienllamo.aquienllamo.model.details.UsuarioSecurity;
 import com.aquienllamo.aquienllamo.model.dtos.Request.DenunciaDTORequest;
 import com.aquienllamo.aquienllamo.model.dtos.Response.DenunciaDTOResponse;
+import com.aquienllamo.aquienllamo.model.entities.ChatEntity;
 import com.aquienllamo.aquienllamo.model.entities.DenunciaEntity;
 import com.aquienllamo.aquienllamo.model.entities.UsuarioEntity;
+import com.aquienllamo.aquienllamo.model.exceptions.ChatNotFoundEx;
 import com.aquienllamo.aquienllamo.model.exceptions.DenunciaNotFoundEx;
 import com.aquienllamo.aquienllamo.model.exceptions.ImageDataTypeNotFoundEx;
 import com.aquienllamo.aquienllamo.model.exceptions.UserNotFoundEx;
 import com.aquienllamo.aquienllamo.model.mappers.DenunciaMapper;
+import com.aquienllamo.aquienllamo.model.repositories.ChatRepository;
 import com.aquienllamo.aquienllamo.model.repositories.DenunciaRepository;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -28,9 +31,10 @@ public class DenunciaService {
     private final DenunciaRepository denunciaRepository;
     private final DenunciaMapper denunciaMapper;
     private final UsuarioRepository usuarioRepository;
+    private final ChatRepository chatRepository;
 
     //crear
-    public DenunciaDTOResponse crearDenuncia(DenunciaDTORequest denuncia){
+    public DenunciaDTOResponse crearDenuncia(DenunciaDTORequest denuncia, String uuidChat){
         //obtener usuario logueado
         UsuarioSecurity usuarioLogueado=(UsuarioSecurity) SecurityContextHolder
                 .getContext()
@@ -40,13 +44,30 @@ public class DenunciaService {
         UsuarioEntity denunciante=usuarioRepository.findByUuid(usuarioLogueado.getUuid())
                 .orElseThrow(()-> new UserNotFoundEx("no se encontro el usuario con ese uuid"));
 
-        //falta denunciado
+        //obtener el chat y sacar el denunciado
+        ChatEntity chat=chatRepository.findByUuidChat(uuidChat)
+                .orElseThrow(()-> new ChatNotFoundEx("el chat con ese uuid no se encontro"));
+
+        UsuarioEntity denunciado;
+        if (chat.getUsuario().getUuid().equals(usuarioLogueado.getUuid())) {
+            denunciado=chat.getTecnico().getUsuario();
+        }else {
+            denunciado=chat.getUsuario();
+        }
+
         DenunciaEntity nueva=denunciaMapper.toEntity(denuncia);
         nueva.setEstadoDenuncia(EstadoDenunciaE.Pendiente);
+        //denunciante
         nueva.setNombreDenunciante(denunciante.getNombre());
         nueva.setApellidoDenunciante(denunciante.getApellido());
         nueva.setDniDenunciante(denunciante.getDni());
         nueva.setTelefonoDenunciante(denunciante.getTelefono());
+        //denunciado
+        nueva.setNombreDenunciado(denunciado.getNombre());
+        nueva.setApellidoDenunciado(denunciado.getApellido());
+        nueva.setDniDenunciado(denunciado.getDni());
+        nueva.setTelefonoDenunciado(denunciado.getTelefono());
+
         nueva.setFechaDenuncia(LocalDateTime.now());
 
         if (denuncia.getFoto()!=null && !denuncia.getFoto().isEmpty()){
