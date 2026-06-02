45596f2b1f756207af9cc35baf05f6af0f9593b6
Paulina006 <paulinasaffer5@gmail.com>
2026-06-02 10:00:48 -0300

    cree UserDetailsServiceImpl
    para cargar los datos del usuario en la bdd.
    Con UserDetailsService le decimos a Spring cómo obtener la información de los usuarios mediante JPA.

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/details/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.aquienllamo.aquienllamo.model.details;
+
+import com.aquienllamo.aquienllamo.model.repositories.CredentialsRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService { //UserDetailsService: es una interfaz de JAVA.
+    private final CredentialsRepository credentialsRepository;
+
+    // para cargar los datos del usuario desde la base de datos.
+    // con UserDetailsService le decimos a Spring cómo obtener la información de los usuarios mediante JPA.
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username)
+            throws UsernameNotFoundException {
+        return
+                credentialsRepository.findByUsername(username).orElseThrow(() -> new
+                        UsernameNotFoundException("User not found"));
+    }
+}
