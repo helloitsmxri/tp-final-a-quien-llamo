fb60831b22daea1c16246c6c0807be2245120cd9
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-06-04 09:39:59 -0300

    update SecurityConfig
    Co-Authored-By: Lour <141468124+helloitsmxri@users.noreply.github.com>

--- src/main/java/com/aquienllamo/aquienllamo/model/auth/SecurityConfig.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/auth/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.aquienllamo.aquienllamo.model.auth;
 
+import com.aquienllamo.aquienllamo.model.auth.JWT.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
@@ -19,14 +27,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // protección contra ataques web.
-                // Le pongo disable porque sino Spring va a rechazar todos los Post, put y delete.
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // permito que se pueda acceder a todo sin autenticación.
-                );
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter, RestAuthenticationEntryPoint restAuthenticationEntryPoint) throws Exception {
+       http.authorizeHttpRequests(auth->
+               auth.requestMatchers("/auth/**").permitAll()
+                       .anyRequest().authenticated())
+               .cors(Customizer.withDefaults())
+               .csrf(AbstractHttpConfigurer::disable)
+               .headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+               .sessionManagement(manager-> manager.sessionCreationPolicy(STATELESS))
+               .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).exceptionHandling(e-> e.authenticationEntryPoint(restAuthenticationEntryPoint));
+       return http.build();
     }
 
     @Bean
@@ -35,5 +45,6 @@ public class SecurityConfig {
             throws Exception {
         return config.getAuthenticationManager();
     }
+
 
 }
