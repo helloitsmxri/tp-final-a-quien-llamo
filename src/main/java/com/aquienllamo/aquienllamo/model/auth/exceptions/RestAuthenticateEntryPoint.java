4b4f032bd645ecda4259d23cb63f01909c537b6e
Lour <mariaelencow@hotmail.com>
2026-06-04 09:46:15 -0300

    Continuación RESTAUTHENTICATEENTRYPOINT
    Finalización de código
    
    Co-Authored-By: Paulina Juana Roldan <141651058+paulinaroldan@users.noreply.github.com>

--- src/main/java/com/aquienllamo/aquienllamo/model/auth/exceptions/RestAuthenticateEntryPoint.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/auth/exceptions/RestAuthenticateEntryPoint.java
@@ -7,8 +7,11 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.security.core.AuthenticationException;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class RestAuthenticateEntryPoint implements AuthenticationEntryPoint {
@@ -23,7 +26,19 @@ public class RestAuthenticateEntryPoint implements AuthenticationEntryPoint {
             case LockedException le-> "Cuenta bloquedad";
             case AccountExpiredException ae->"Cuenta expirada";
             case CredentialsExpiredException cee-> "Credenciales expiradas";
-        }
+            case InsufficientAuthenticationException ie -> "Autenticación insuficiente";
+            case AuthenticationServiceException ae -> "Error en el servicio de autenticación";
+            default -> "Error de autenticación: "+ authException.getMessage();
+        };
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("error", errorMessage);
+        responseData.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        responseData.put("path", request.getRequestURI()); // ¿por qué uri y no url?
+
+        response.getWriter().write(mapper.writeValueAsString(responseData));
+        response.getWriter().flush();
     }
 
 
