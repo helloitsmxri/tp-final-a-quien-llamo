4b64a7d762f86366337039646b8817aff6f05cbe
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-06-14 12:16:36 -0300

    prueba

--- src/main/java/com/aquienllamo/aquienllamo/model/entities/CertificacionEntity.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/entities/CertificacionEntity.java
@@ -50,4 +50,5 @@ public class CertificacionEntity {
     @Lob
     @Column(nullable = false, name = "imagen_certificado", columnDefinition = "MEDIUMBLOB")
     private byte[] imagenCertificado;
+    //prueba
 }
