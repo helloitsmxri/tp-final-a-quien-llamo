507e66ae90d8682be30969de57a40822368c5689
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-06-14 16:30:39 -0300

    cree excepciones DenunciaResueltaEx y AdminAsignadoDenunciaEx
    Co-Authored-By: Lour <141468124+helloitsmxri@users.noreply.github.com>

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/AdminAsignadoDenunciaEx.java
@@ -0,0 +1,7 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class AdminAsignadoDenunciaEx extends RuntimeException {
+    public AdminAsignadoDenunciaEx(String message) {
+        super(message);
+    }
+}
