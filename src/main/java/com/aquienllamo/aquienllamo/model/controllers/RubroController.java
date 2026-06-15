3629a56f459b4f7b70d19c8a97ecca8c3901a1e7
IrinaB7 <irinabenvenutto@gmail.com>
2026-06-15 14:35:33 -0300

    update rubro
    cree mas metodos en el service y controller para pder modificar crear y eliminar rubros, pero esto solo lo va a poder hacer el administrador

--- src/main/java/com/aquienllamo/aquienllamo/model/controllers/RubroController.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/controllers/RubroController.java
@@ -1,10 +1,13 @@
 package com.aquienllamo.aquienllamo.model.controllers;
 
+import com.aquienllamo.aquienllamo.model.dtos.Request.RubroDTORequest;
 import com.aquienllamo.aquienllamo.model.dtos.Response.RubroDTOResponse;
 import com.aquienllamo.aquienllamo.model.services.RubroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +19,53 @@ public class RubroController {
 
     private final RubroService rubroService; //el controller usa el servicio para hacer la logica
 
+    //crear un rubro (solo lo puede hacer admin)
+    @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    public ResponseEntity<RubroDTOResponse> crearRubro(@Valid @RequestBody RubroDTORequest rubroDTORequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(rubroService.crearRubro(rubroDTORequest));
+    }
+
+    //modificar un rubro (solo lo puede hacer el admin)
+    @PutMapping("/{uuid}")
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    public ResponseEntity<RubroDTOResponse> modificarRubro (@PathVariable String uuid, @Valid @RequestBody RubroDTORequest rubroDTORequest){
+        return ResponseEntity.ok(rubroService.modificarRubro(uuid, rubroDTORequest));
+    }
+
+    //eliminar un rubro (solo el admin)
+    @DeleteMapping("/{uuid}")
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    public ResponseEntity<Void> eliminarRubro(@PathVariable String uuid){
+        rubroService.eliminarRubro(uuid);
+        return ResponseEntity.noContent().build(); //204 No Content, operación exitosa, pero no tengo nada para devolverte
+    }
+
     //obtener todos los rubros
     @GetMapping
+    @PreAuthorize("isAuthenticated()") //cualquier usuario logueado puede ver los rubros disponibles
     public ResponseEntity<List<RubroDTOResponse>> getAllRubros(){
         return ResponseEntity.ok().body(rubroService.getAllRubros());
     }
 
     //busco rubro por uuid
     @GetMapping("/{uuid}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<RubroDTOResponse> getRubroByUuid(@Valid @PathVariable String uuid){
         return ResponseEntity.ok().body(rubroService.getRubroByUuid(uuid));
     }
 
     //buscar nombre exacto de rubro
     @GetMapping("/nombre/{nombre}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<RubroDTOResponse> getRubroByNombre(@PathVariable String nombre){
         return ResponseEntity.ok().body(rubroService.getRubroByNombre(nombre));
     }
 
     //buscar un rubro, puede ser por el nombre entero o por parte del nombre ej tec = tecnico
     @GetMapping("/buscar")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<RubroDTOResponse>> findRubros(@RequestParam String nombre){
         return ResponseEntity.ok().body(rubroService.findRubroContaining(nombre));
     }
-
-
-
-
 }
--- src/main/java/com/aquienllamo/aquienllamo/model/services/RubroService.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/services/RubroService.java
@@ -1,7 +1,12 @@
 package com.aquienllamo.aquienllamo.model.services;
+import com.aquienllamo.aquienllamo.model.dtos.Request.RubroDTORequest;
 import com.aquienllamo.aquienllamo.model.dtos.Response.RubroDTOResponse;
 import com.aquienllamo.aquienllamo.model.entities.RubroEntity;
+import com.aquienllamo.aquienllamo.model.exceptions.EspecialidadNotFoundEx;
+import com.aquienllamo.aquienllamo.model.exceptions.RubroAlreadyExistsEx;
+import com.aquienllamo.aquienllamo.model.exceptions.RubroNotFoundEx;
 import com.aquienllamo.aquienllamo.model.mappers.RubroMapper;
+import com.aquienllamo.aquienllamo.model.repositories.EspecialidadRepository;
 import com.aquienllamo.aquienllamo.model.repositories.RubroRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +21,38 @@ public class RubroService {
 
     private final RubroMapper mapper;
     private final RubroRepository rubroRepository;
+    private final EspecialidadRepository especialidadRepository;
+
+    //crear un rubro (esto le va a servir a admin en un futuro que quiera agregar rubros)
+    public RubroDTOResponse crearRubro(RubroDTORequest request){
+        if(rubroRepository.existsByNombreRubro(request.getNombreRubro())){
+            throw new RubroAlreadyExistsEx("ERROR: El rubro "+request.getNombreRubro()+" ya existe.");
+        }
+        RubroEntity rubro = mapper.toEntity(request);
+        return mapper.toResponseRubro(rubroRepository.save(rubro));
+    }
+
+    //modificar rubro (solo lo puede hacer el admin)
+    public RubroDTOResponse modificarRubro(String uuid,RubroDTORequest request){
+        RubroEntity rubro = rubroRepository.findByUuid(uuid)
+                .orElseThrow(()-> new RubroNotFoundEx("ERROR: El rubro ingresado no existe."));
+        rubro.setNombreRubro(request.getNombreRubro());
+        rubro.setEspecialidades(
+                request.getUuidEspecialidad()
+                        .stream()
+                        .map(especialidad -> especialidadRepository.findByUuid(especialidad)
+                                .orElseThrow(()-> new EspecialidadNotFoundEx("ERROR: La especialidad " + especialidad + " no existe.")))
+                        .toList()
+        );
+        return mapper.toResponseRubro(rubroRepository.save(rubro));
+    }
+
+    //eliminar un rubro
+    public void eliminarRubro(String uuid){
+        RubroEntity rubro = rubroRepository.findByUuid(uuid)
+                .orElseThrow(()-> new RubroNotFoundEx("ERROR: El rubro ingresado no existe."));
+        rubroRepository.delete(rubro);
+    }
 
     //enlistar los rubros
     public List<RubroDTOResponse> getAllRubros(){
@@ -28,14 +65,14 @@ public class RubroService {
     //buscar rubro por uuid
     public RubroDTOResponse getRubroByUuid(String uuid){
         RubroEntity rubro = rubroRepository.findByUuid(uuid)
-                .orElseThrow();
+                .orElseThrow(()-> new RubroNotFoundEx("ERROR: El rubro ingresado no existe."));
         return mapper.toResponseRubro(rubro);
     }
 
     //buscar rubro por nombre
     public RubroDTOResponse getRubroByNombre(String nombre){
         RubroEntity rubro = rubroRepository.findByNombreRubro(nombre)
-                .orElseThrow();
+                .orElseThrow(()-> new RubroNotFoundEx("ERROR: El rubro "+nombre+" no existe."));
         return mapper.toResponseRubro(rubro);
     }
 
