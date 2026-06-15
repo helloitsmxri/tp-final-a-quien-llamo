030a4e9d3b6aa95cc6f0a3df598c9ea329f73728
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-06-15 13:04:47 -0300

    update HabilidadRepository
    le agregue existsByUuid y findAllByUuidIn

--- src/main/java/com/aquienllamo/aquienllamo/model/repositories/HabilidadRepository.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/repositories/HabilidadRepository.java
@@ -4,9 +4,12 @@ import com.aquienllamo.aquienllamo.model.entities.HabilidadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HabilidadRepository extends JpaRepository<HabilidadEntity,Integer> {
     Optional<HabilidadEntity> findByUuid(String uuid);
+    boolean existsByUuid(String uuid);
+    List<HabilidadEntity> findAllByUuidIn(List<String> uuids);
 }
