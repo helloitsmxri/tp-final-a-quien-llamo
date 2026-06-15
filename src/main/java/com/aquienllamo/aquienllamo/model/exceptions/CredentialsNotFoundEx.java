3d67d92f1c646e5d54de94159b1ceb5fefaf7c3e
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-06-15 12:35:19 -0300

    cree la excepcion CredentialsNotFoundEx

---
+++ src/main/java/com/aquienllamo/aquienllamo/model/exceptions/CredentialsNotFoundEx.java
@@ -0,0 +1,7 @@
+package com.aquienllamo.aquienllamo.model.exceptions;
+
+public class CredentialsNotFoundEx extends RuntimeException {
+    public CredentialsNotFoundEx(String message) {
+        super(message);
+    }
+}
