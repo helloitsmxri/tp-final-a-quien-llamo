8671d10cb56848658695dc4b0bf7f813ab93108d
IrinaB7 <irinabenvenutto@gmail.com>
2026-06-15 16:13:05 -0300

    update portfolio
    agregue metodos que le sirven a la rama admin como el de rechazar o aprobar portfolio

--- src/main/java/com/aquienllamo/aquienllamo/model/controllers/PortfolioAdminController.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/controllers/PortfolioAdminController.java
@@ -7,6 +7,7 @@ import com.aquienllamo.aquienllamo.model.services.PortfolioAdminService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -20,24 +21,28 @@ public class PortfolioAdminController {
     private final PortfolioAdminService service;
 
     //buscar un portfolio por uuid
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/{uuid}")
     public ResponseEntity<PortfolioAdminDTOResponse> buscarPortfolioPorUuid(@PathVariable String uuid){
         return ResponseEntity.ok(service.buscarPorfolioPorUuid(uuid));
     }
 
     //ver todos los portfolios
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping
     public ResponseEntity<List<PortfolioAdminDTOResponse>> listarPortfolios(){
         return ResponseEntity.ok().body(service.listarPortfolios());
     }
 
     //ver todos los portfolios con un estado en particular
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/estadoVerificacion")
     public ResponseEntity<List<PortfolioAdminDTOResponse>> listarPortfoliosPorEstado(@RequestParam EstadoVerificacion estadoVerificacion){
         return ResponseEntity.ok().body(service.listarPorEstado(estadoVerificacion));
     }
 
     //ver todos los portfolios dentro de un rango de tiempo
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/fecha")
     public ResponseEntity<List<PortfolioAdminDTOResponse>> listarPortfoliosPorRangoDeTiempo(
             @RequestParam LocalDateTime desde,
@@ -46,22 +51,30 @@ public class PortfolioAdminController {
     }
 
     //ver todos los portfolios de una especialidad en particular
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/especialidad/{nombreEspecialidad}")
     public ResponseEntity<List<PortfolioAdminDTOResponse>> listarPortfoliosPorEspecialidad(@PathVariable String nombreEspecialidad){
         return ResponseEntity.ok().body(service.listarPorEspecialidad(nombreEspecialidad));
     }
 
     //ver portfolios de un tecnico
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/tecnico/{uuidTecnico}")
     public ResponseEntity<List<PortfolioAdminDTOResponse>> listarPortfoliosDeTecnico(@PathVariable String uuidTecnico){
         return ResponseEntity.ok().body(service.obtenerTecnicoPorUuid(uuidTecnico));
     }
 
-    //revision de portfolio
-    @PatchMapping("/{uuid}")
-    public ResponseEntity<PortfolioAdminDTOResponse> revisionPortfolio (@PathVariable String uuid, @Valid @RequestBody PortfolioAdminDTORequest request){
-        return ResponseEntity.ok().body(service.revisionPortfolio(uuid, request));
+    //aprobar portfolio
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @PatchMapping("/aprobar/{uuid}")
+    public ResponseEntity<PortfolioAdminDTOResponse> aprobarPorfolio(@PathVariable String uuid, @RequestParam(required = false) String notasAdmin){
+        return ResponseEntity.ok(service.aprobarPortfolio(uuid, notasAdmin));
     }
 
-
+    //rechazar portfolio
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @PatchMapping("/rechazar/{uuid}")
+    public ResponseEntity<PortfolioAdminDTOResponse> rechazarPorfolio(@PathVariable String uuid, @RequestParam String notasAdmin){
+        return ResponseEntity.ok(service.rechazarPortfolio(uuid, notasAdmin));
+    }
 }
