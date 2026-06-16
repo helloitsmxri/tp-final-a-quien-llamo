0535254a33250ba80e66d2158163c5184621f333
Paulina006 <paulinasaffer5@gmail.com>
2026-06-16 00:09:20 -0300

    corregí error
    El UsuarioDetails pisaba al UserDetailsService que hicimos en el Auth, entonces al logear a un administrador, esto nunca se hacia ya que se tenía dos Services de usuarioDetails

--- src/main/java/com/aquienllamo/aquienllamo/model/services/UsuarioDetails.java
+++ src/main/java/com/aquienllamo/aquienllamo/model/services/UsuarioDetails.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Service
+//@Service
 public class UsuarioDetails implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
